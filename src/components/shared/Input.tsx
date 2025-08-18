6449fe9995073822622b83473b8bb46ea0c3ba77
Whisil <electromandarin@gmail.com>
2025-08-18 17:45:06 +0300

    implement input component

---
+++ src/components/shared/Input.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import clsx from "clsx"
+import { ChangeEvent, InputHTMLAttributes, useState } from "react"
+
+type InputProps = {
+  value: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+} & Omit<InputHTMLAttributes<HTMLInputElement>, "value">
+
+const Input = ({ value, onChange, ...props }: InputProps) => {
+  const [hidePassword, setHidePassword] = useState(true)
+
+  return (
+    <label
+      className={clsx(
+        "rounded-lg border-2 border-transparent bg-white-mellow px-3 py-2.5 transition-colors",
+        "flex w-full items-center justify-between gap-4 focus-within:border-purple-default",
+      )}
+    >
+      <input
+        {...props}
+        type={
+          props.type === "password"
+            ? hidePassword
+              ? "password"
+              : "text"
+            : props.type
+        }
+        value={value}
+        onChange={onChange}
+        className={clsx(
+          "leading-0 w-full !bg-transparent text-black-default outline-none",
+          "focus:ring-0",
+        )}
+      />
+      {props.type === "password" && (
+        <button
+          type="button"
+          className={clsx(
+            "py-1 font-montserratAlt text-sm font-black",
+            "leading-none text-black-default opacity-50",
+          )}
+          onClick={() => setHidePassword((prev) => !prev)}
+        >
+          {hidePassword ? "Show" : "Hide"}
+        </button>
+      )}
+    </label>
+  )
+}
+
+export default Input
