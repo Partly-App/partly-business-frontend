9b198c555772c675b58d759a2b4cea54322ed64d
Whisil <electromandarin@gmail.com>
2025-08-25 23:57:08 +0300

    implement modal component

---
+++ src/components/shared/Modal.tsx
@@ -0,0 +1,72 @@
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react"
+import { Dispatch, Fragment, ReactNode, SetStateAction } from "react"
+import { X } from "react-feather"
+import Button from "./Button"
+
+type ModalProps = {
+  isOpen: boolean
+  setIsOpen: Dispatch<SetStateAction<boolean>>
+  title?: string
+  children: ReactNode
+}
+
+const Modal = ({ isOpen, setIsOpen, title, children }: ModalProps) => {
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-50"
+        onClose={() => setIsOpen(false)}
+      >
+        <TransitionChild
+          as={Fragment}
+          enter="ease-out duration-200"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black-default/30" />
+        </TransitionChild>
+
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <TransitionChild
+            as={Fragment}
+            enter="ease-out duration-200"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-150"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <DialogPanel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-grey-dark px-6 py-4 text-left align-middle shadow-xl transition-all">
+              <div className="flex items-start justify-between gap-4">
+                <h3 className="text-wrap font-montserratAlt text-3xl font-black">
+                  {title}
+                </h3>
+                <Button
+                  containsIconOnly
+                  size="XS"
+                  color="transparent"
+                  containerClassName="shrink-0"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <X size={16} color="white" />
+                </Button>
+              </div>
+              {children}
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
+
+export default Modal
